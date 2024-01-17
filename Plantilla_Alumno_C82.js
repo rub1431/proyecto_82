f2608cd7097e72bef2df5bc67fde169cd6f3677a
rub1431 <143140625+rub1431@users.noreply.github.com>
2024-01-16 18:00:32 -0600

    Add files via upload

---
+++ Plantilla_Alumno_C82.js
@@ -0,0 +1,41 @@
+console.log("My first console project");
+
+            var first_name = "Ranbir";
+            var last_name = "Kapoor";
+
+
+            var full_name = first_name.concat(last_name);
+            console.log(full_name);
+
+            var a = 10;
+            var b = 5;
+
+            var add = a + b;
+            console.log( add);
+
+/*
+Crear una variable llamada sub y guardar aquí la el valor de la resta de a y b.
+
+Escribir: var sub = a - b
+
+Después, imprimie el valor para mostrar el valor de la resta.
+
+Escribir: console.log(sub)
+*/
+            var sub = a - b;
+            console.log(sub);
+
+/*
+Crear una variable llamada multiple y guardar aquí el valor de la multiplicación de a y b.
+
+Escribir: var multiple = a * b
+
+Después, imprime la variable para mostrar el valor de la multiplicación.
+
+Escribir: console.log(multiple)
+*/
+            var multiple = a * b;
+            console.log(multiple);
+
+            var divide = a / b ;
+            console.log(divide);
